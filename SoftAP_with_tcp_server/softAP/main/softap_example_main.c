37ed9d3d8ba14c0ab6bed3b8850dbe6b488764df
Julian Caredda <jcaredda@student-net.ethz.ch>
2024-11-19 16:28:42 +0100

    testing

--- SoftAP_with_tcp_server/softAP/main/softap_example_main.c
+++ SoftAP_with_tcp_server/softAP/main/softap_example_main.c
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+//Testing
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
